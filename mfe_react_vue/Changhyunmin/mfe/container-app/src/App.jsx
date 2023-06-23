6e3e7507af5fa794e9448d38e5fb34d8fd79c5da
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-06-23 09:12:17 +0900

    init: [장현민]container-app[MFE-1]

---
+++ mfe_react_vue/Changhyunmin/mfe/container-app/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from "react";
+import { mount } from 'vueModuleApp/Sample';
+import { navigationMount } from 'vueNavigationApp/VueNavigation';
+import Counter from 'reactModuleApp/Counter';
+import BoardApp from 'reactBoardApp/BoardApp'
+import { BrowserRouter } from 'react-router-dom'
+
+const App = () => {
+  const vueRef = useRef(null)
+  const vuetifyNavigationRef = useRef(null)
+
+  useEffect(() => {
+    mount(vueRef.current)
+    navigationMount(vuetifyNavigationRef.current)
+  }, []);
+
+  return (
+    <div>
+      <div style={{ zIndex: 999 }} ref={vuetifyNavigationRef}/>
+      <div style=
+          {{ margin: '10px', padding: '10px', textAlign: 'center',
+              backgroundColor: 'cyan', position: 'relative', zIndex: 1 }}>
+        <h1>나는 React로 만들어진 Container!</h1>
+        <div ref={vueRef}/>
+      </div>
+      <Counter/>
+      <BrowserRouter>
+        <BoardApp/>
+      </BrowserRouter>
+    </div>
+  )
+};
+
+export default App;
